363635b325db285beaf7f306b15649d786035f46
Alejandro Villarreal <avillarreal01@gmail.com>
2017-09-18 18:20:43 -0400

    added middleware

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+//Middleware
+app.use(morgan('tiny'));
+app.use('/special/', (req, res, next) => {
+  console.log('You are someone special.');
+  next();
+});
+
+//Routing
 app.get('/', (req, res) => {
   res.send('Hey! Welcome home!');
 });
