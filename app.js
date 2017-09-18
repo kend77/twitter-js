672d156e991d3b28e46159503975205dd4406c9e
Alejandro Villarreal <avillarreal01@gmail.com>
2017-09-18 18:33:36 -0400

    add nunjucks

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 const app = express();
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
 
 //Middleware
 app.use(morgan('tiny'));
@@ -12,6 +17,19 @@ app.use('/special/', (req, res, next) => {
 //Routing
 app.get('/', (req, res) => {
   res.send('Hey! Welcome home!');
+});
+
+var locals = {
+  title: 'An Example',
+  people: [
+    {name: 'Gandalf'},
+    {name: 'Frodo'},
+    {name: 'Hermione'}
+  ]
+};
+nunjucks.render('index.html', locals, function (err, output) {
+  if (err) throw err;
+  console.log(output);
 });
 
 app.listen(3000, function() {
